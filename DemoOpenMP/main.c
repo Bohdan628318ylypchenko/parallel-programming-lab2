08cc2828267d00497136c6e68318a2838cf3305e
Bohdan Pylypchenko <bogdan314ylypchenko@gmail.com>
2023-10-30 14:02:39 +0200

    Task 1 implementation: - check for openmp support - determine timer properties - parallel hello world

--- DemoOpenMP/main.c
+++ DemoOpenMP/main.c
@@ -1,4 +1,24 @@
+#include <omp.h>
+#include <stdio.h>
+
 int main()
 {
+	#ifdef _OPENMP
+	puts("OpenMP is supported.");
+	#endif
+
+	long double start_time, end_time, tick;
+	start_time = omp_get_wtime();
+	end_time = omp_get_wtime();
+	tick = omp_get_wtick();
+
+	printf("end_time - start_time = %lf; tick = %lf\n", end_time - start_time, tick);
+
+	#pragma omp parallel
+	{
+		int thread_num = omp_get_thread_num();
+		printf("Hello world from thread with thread_num = %d\n", thread_num);
+	}
+
 	return 0;
 }
